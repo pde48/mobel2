ac8174c01de2b35d919088adf8ddc9b9d3c4d214
rafaelcabatel <86730585+rafaelcabatel@users.noreply.github.com>
2021-08-23 12:10:04 -0500

    update 23082021

--- mobel/models/__init__.py
+++ mobel/models/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from . import res_company
 from . import mobel_sales
 from . import mobel_product
 from . import res_config_setting_sales
 from . import mobel_purchase_import
 from . import mobel_stock
-from . import res_company
 from . import mobel_exception_purchase_tracing
